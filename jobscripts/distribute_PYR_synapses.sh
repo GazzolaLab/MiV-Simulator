9c2318e19ffb2497138e3d43ff8a77063fe2e2cb
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-03-28 10:32:36 -0700

    added back HC layer definitions in order to enable synaptic distribution

--- jobscripts/distribute_PYR_synapses.sh
+++ jobscripts/distribute_PYR_synapses.sh
@@ -6,4 +6,4 @@ mpirun.mpich -np 1 python3 ./scripts/distribute_synapse_locs.py \
               --forest-path=./datasets/PYR_forest.h5 \
               --output-path=./datasets/PYR_forest.h5 \
               --distribution=poisson \
-              --io-size=1 -v
+              --io-size=1 -v --dry-run
