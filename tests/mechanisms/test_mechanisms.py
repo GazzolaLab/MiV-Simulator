3da3a2fbdb332e1ce862fd6f7b6e3d20e17055cf
Frithjof Gressmann <fg14@illinois.edu>
2022-10-11 22:22:25 -0500

    AChange directories for default morphology and mechanisms

--- tests/mechanisms/test_mechanisms.py
+++ tests/mechanisms/test_mechanisms.py
@@ -5,7 +5,7 @@ import commandlib
 from miv_simulator.mechanisms import compile, compile_and_load
 
 
-def test_compile():
+def test_mechanisms_compile():
     # compile with -force
     compiled_path = compile(force=True)
     assert os.path.basename(compiled_path) == "compiled"
@@ -14,7 +14,7 @@ def test_compile():
     compile()
 
 
-def test_compile_and_load():
+def test_mechanisms_compile_and_load():
     # Test if compile_and_load is called properly
     dll_path = compile_and_load()
     assert os.path.exists(dll_path)
