a20e09054996bee6c039c15d2d88b8f1be0595bd
Ivan Raikov <ivan.g.raikov@gmail.com>
2023-07-20 11:10:41 -0700

    refactoring encoder for 1-D input signal for now

--- src/miv_simulator/spike_encoder.py
+++ src/miv_simulator/spike_encoder.py
@@ -1,12 +1,12 @@
 import numpy as np
 from numpy import ndarray
-from typing import Tuple
+from typing import Tuple, Optional
 
 
 class RateEncoder:
     def __init__(
         self,
-        time_window: int = 100,
+        dt: float = 0.02,
         input_range: Tuple[int, int] = (0, 255),
         output_freq_range: Tuple[int, int] = (0, 200),
     ) -> None:
@@ -19,7 +19,8 @@ class RateEncoder:
             output_freq_range[1],
         )
         self.time_window = time_window
-
+        self.ndim = 1
+        
     def encode(self, signal: ndarray) -> ndarray:
         assert (
             len(signal.shape) == 2
@@ -27,26 +28,23 @@ class RateEncoder:
 
         nsamples = signal.shape[0]
         ndim = signal.shape[1]
-
-        total_spikes = []
-        for r in range(ndim):
-            s = signal[:, r]
-            freq = np.interp(
-                s,
+        assert ndim == self.ndim, f"input signal has dimension {ndim} but encoder has input dimension {self.ndim}" 
+        
+        freq = np.interp(
+                signal,
                 [self.min_input, self.max_input],
                 [self.min_output, self.max_output],
             )
-            nz = np.argwhere(freq > 0)
-            period = np.zeros(nsamples)
-            period[nz] = (1 / freq[nz]) * 1000  # ms
-            spikes = np.zeros((nsamples, self.time_window))
-            for i in range(nsamples):
-                if period[i] > 0:
-                    stride = int(period[i])
-                    spikes[i, 0 : self.time_window : stride] = 1
-            total_spikes.append(spikes)
+        nz = np.argwhere(freq > 0)
+        period = np.zeros(nsamples)
+        period[nz] = (1 / freq[nz]) * 1000  # ms
+        spikes = np.zeros((nsamples, self.time_window))
+        for i in range(nsamples):
+            if period[i] > 0:
+                stride = int(period[i])
+                spikes[i, 0 : self.time_window : stride] = 1
 
-        return np.stack(total_spikes, axis=1)
+        return spikes
 
 
 class PoissonRateEncoder:
@@ -55,7 +53,7 @@ class PoissonRateEncoder:
         time_window: int = 100,
         input_range: Tuple[int, int] = (0, 255),
         output_freq_range: Tuple[int, int] = (0, 200),
-        generator: None = None,
+        generator: Optional[np.random.RandomState] = None,
     ) -> None:
         assert input_range[1] - input_range[0] > 0
         assert output_freq_range[1] - output_freq_range[0] > 0
@@ -69,7 +67,8 @@ class PoissonRateEncoder:
         if generator is None:
             generator = np.random
         self.generator = generator
-
+        self.ndim = 1
+        
     def encode(self, signal: ndarray) -> ndarray:
         assert (
             len(signal.shape) == 2
@@ -77,25 +76,21 @@ class PoissonRateEncoder:
 
         nsamples = signal.shape[0]
         ndim = signal.shape[1]
+        assert ndim == self.ndim, f"input signal has dimension {ndim} but encoder has input dimension {self.ndim}" 
 
-        total_spikes = []
-        for r in range(ndim):
-            spike_train = []
-            s = signal[:, r]
-            freq = np.interp(
-                s,
-                [self.min_input, self.max_input],
-                [self.min_output, self.max_output],
-            )
+        spike_train = []
+        freq = np.interp(
+            signal,
+            [self.min_input, self.max_input],
+            [self.min_output, self.max_output],
+        )
 
-            spikes = np.random.uniform(
-                0, 1, nsamples * self.time_window
-            ).reshape((nsamples, self.time_window))
-            dt = 0.001  # second
-            for i in range(nsamples):
-                spikes[i, np.where(spikes < freq[i] * dt)] = 1
-                spikes[i, np.where(spikes[i] != 1)] = 0
+        spikes = self.generator.uniform(
+            0, 1, nsamples * self.time_window
+        ).reshape((nsamples, self.time_window))
+        dt = 0.001  # second
+        for i in range(nsamples):
+            spikes[i, np.where(spikes < freq[i] * dt)] = 1
+            spikes[i, np.where(spikes[i] != 1)] = 0
 
-            total_spikes.append(spikes)
-
-        return np.stack(total_spikes, axis=1)
+        return spikes
